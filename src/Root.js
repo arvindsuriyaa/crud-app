aedbed381bbe02c7a31ae1867e2e51b18a64abec
arvindsuriyaa <arvinds2605@gmail.com>
2020-05-29 10:27:40 +0530

    root file modified

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,62 @@
 import React, { Component } from "react";
-import LayoutContainer from "./Container/LayoutContainer";
-
+import EmployeeForm from "./Component/EmployeeForm";
+import Table from "./Component/Table";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 class Root extends Component {
   render() {
-    return <LayoutContainer />;
+    return (
+      <Router>
+        <div id="title">
+          <span>Employee Details</span>
+          <div className="link">
+            <NavLink
+              activeStyle={{
+                color: "black",
+                backgroundColor: "white",
+              }}
+              to="/Table"
+            >
+              Table
+            </NavLink>
+            <NavLink
+              activeStyle={{
+                color: "black",
+                backgroundColor: "white",
+              }}
+              to="/Form"
+            >
+              Form
+            </NavLink>
+          </div>
+        </div>
+        <div className="App-intro">
+          <Switch>
+            <Route exact path="/Table" />
+            <Route path="/Form" />
+            <Redirect to="/Table" />
+          </Switch>
+        </div>
+        <Route
+          path="/Form"
+          exact
+          strict
+          render={() => {
+            return <EmployeeForm />;
+          }}
+        />
+        <Route
+          path="/Table"
+          exact
+          strict
+          render={() => {
+            return <Table />;
+          }}
+        />
+      </Router>
+    );
   }
 }
+
 export default Root;
